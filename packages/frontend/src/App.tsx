ea1cf6259feafbf0df48fd6366bc70c3d61c9ea8
Mikołaj Duchewicz <mikolaj.duchewicz@mug.pl>
2021-01-29 21:04:43 +0100

    up

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       {Test.Siema}
-      {cos}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
