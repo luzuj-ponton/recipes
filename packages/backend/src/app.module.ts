9b089df4bc83eb512534072961b827e367212fcc
Mikołaj Duchewicz <mikolaj.duchewicz@mug.pl>
2021-01-30 13:47:07 +0100

    configure mongoose

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.DB_URL as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
