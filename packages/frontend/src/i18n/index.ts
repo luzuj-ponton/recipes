fcce4cfa718be5bef7cc204b45689793a37c8b76
AdamWojty <adam12573@gmail.com>
2021-02-06 19:08:27 +0100

    feature/translations mr fix

--- packages/frontend/src/i18n/index.ts
+++ packages/frontend/src/i18n/index.ts
@@ -2,26 +2,15 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationsPL from "../assets/translations/plTranslations.json";
 import translationsEN from "../assets/translations/enTranslations.json";
-
-enum Languages {
-  Polish = "pl-PL",
-  English = "en-US",
-}
-
-enum LanguagesTypes {
-  Polish = "pl",
-  English = "en",
-}
+import { Languages, LanguagesTypes } from "src/enums/languages.enum";
 
 const language = window.navigator.language;
 
-const resources = {
-  pl: translationsPL,
-  en: translationsEN,
-};
-
 i18n.use(initReactI18next).init({
-  resources,
+  resources: {
+    pl: translationsPL,
+    en: translationsEN,
+  },
   lng: language === Languages.Polish ? LanguagesTypes.Polish : LanguagesTypes.English,
 });
 
