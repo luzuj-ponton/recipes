f443850608e0f070e6dafa32544660e12686bd45
Adam <58430648+Adamwojty@users.noreply.github.com>
2021-02-06 19:52:20 +0100

    [feature/translations] Tłumaczenia (#6)
    * feature/translations Add i18next, react-i18next, init i18n, add basic translations
    
    * feature/translations mr fix

--- packages/frontend/src/Layout/Nav/Nav.tsx
+++ packages/frontend/src/Layout/Nav/Nav.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
+import i18n from "i18next";
+import { useTranslation } from "react-i18next";
 import {
   Wrapper,
   Hamburger,
@@ -15,6 +17,7 @@ import {
   SocialsWrapper,
   SocialIcon,
 } from "./Nav.styled";
+
 import { Button } from "../../components/Button/Button";
 import { Routes } from "../../enums/routes.enum";
 import { useLocation } from "react-router-dom";
@@ -24,13 +27,13 @@ import facebookIcon from "../../assets/images/facebook.svg";
 import githubIcon from "../../assets/images/github.svg";
 
 const links = [
-  { text: "Home", path: Routes.Home },
-  { text: "Add recipe", path: Routes.AddRecipe },
+  { text: i18n.t("common:labels.home"), path: Routes.Home },
+  { text: i18n.t("common:labels.add-recipe"), path: Routes.AddRecipe },
 ];
 
 export const Nav: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
+  const { t } = useTranslation();
   const { pathname } = useLocation();
 
   const toggleNav = () => {
@@ -61,7 +64,7 @@ export const Nav: React.FC = () => {
           })}
         </NavItems>
         <Link to={Routes.Register}>
-          <Button variant="primary">Sign up</Button>
+          <Button variant="primary">{t("common:actions.sign-up")}</Button>
         </Link>
         <SocialsWrapper>
           <SocialIcon src={linkedinIcon} />
